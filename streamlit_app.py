5b87c05444ad249197e657de6855a5543dde9d01
basvanderworp <bas.van.der.worp@navara.nl>
2023-02-08 11:33:26 +0100

    tidy up and streamlit.stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healty Diner')
 
@@ -41,6 +42,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfuit')
 streamlit.write('Thanks for adding ', add_my_fruit)
